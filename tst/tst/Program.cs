5cd027cfd1235248eb520f39743e15ca01062087
sohelarman1100 <armanhossain54546@gmail.com>
2021-04-12 00:19:56 +0600

    adding few codes

--- tst/tst/Program.cs
+++ tst/tst/Program.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 
 namespace tst
 {
-    class Program
+    public class Program
     {
+        public int add(params int[] ara)
+        {
+            int i, res = 0;
+            for(i=0; i<ara.Length;i++)
+            {
+                res += ara[i];
+            }
+            return res;
+        }
         static void Main(string[] args)
         {
             int k,i,j,m,n;
-            List<int> lst = new List<int>();
+            //List<int> lst = new List<int>();
             var inp = Console.ReadLine();
             var data = inp.Split(' ');
-            k = inp.Length;
-            Console.WriteLine(k);
+            //k = inp.Length;
+            //Console.WriteLine(k);
             k = data.Length;
+            int[] lst = new int[k];
             for(i=0; i<k; i++)
             {
                 m = int.Parse(data[i]);
-                lst.Add(m);
+                lst[i] = m;
             }
-            //Console.WriteLine(k);
+            Program obj = new Program();
+            m = obj.add(lst);
+            Console.WriteLine(m);
 
         }
     }
